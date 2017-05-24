650e3ac8ac8b9f146ea80c560136dd0301d44d18
Debra Gordon <debra.m.gordon@gmail.com>
2017-05-23 18:46:30 -0700

    added angular

---
+++ FamilyApp/Models/Family.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FamilyApp.Models
+{
+    public class Family
+    {
+        [Key]
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string Origin { get; set; }
+        public virtual List<Individual> Members { get; set; }
+
+    }
+}
